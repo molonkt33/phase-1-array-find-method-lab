38a8fd8d6d359b688512f24211d694b848ba38e1
caleb <kalebntoyian2004@gmail.com>
2024-07-23 16:11:06 +0300

    phase-1-array-find-method-lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// code your solution here
+// index.js
+
+// Function to find the year Denver Broncos won the Super Bowl
+function superbowlWin(records) {
+    const winRecord = records.find(record => record.result === "W");
+    return winRecord ? winRecord.year : undefined;
+  }
+  
